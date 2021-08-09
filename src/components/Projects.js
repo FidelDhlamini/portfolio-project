bb78d6cbe17a439477636c140f46ccf5fec9efd6
Fidel <goodkidfidel@gmail.com>
2021-08-09 17:41:07 +0200

    added github links to all project repositories

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,6 +34,7 @@ function Projects() {
     imageUrl= {setbill}
     body="In this web app you first update the cost for call and sms and at what total should it should warn you,critical level then you update settings,select the call or sms radio button and start adding.The total should change colour when you reach the warning and critical levels"
     link="https://settings-bill-expressj.herokuapp.com/"
+    githubLink="https://github.com/FidelDhlamini/settings-bill-expressjs"
     
     />
     </div>
@@ -44,6 +45,7 @@ function Projects() {
     imageUrl={regnum}
     body="This is a web app for storing Western Cape vehicle registration numbers from the regions Stellenbosch(CL),Belville(CF) and Cape Town(CA) "
     link="https://regnumbers-app.herokuapp.com/"
+    githubLink="https://github.com/FidelDhlamini/registration_numbers_webapp"
     />
      </div>
 
@@ -65,6 +67,7 @@ function Projects() {
     title="Rest-Bafe2"
     imageUrl=""
     body=""
+    githubLink="https://github.com/FidelDhlamini/RestBafe2/tree/Fidel"
     />
     </div>
 
@@ -76,6 +79,7 @@ function Projects() {
     title="H20-Leveler"
     imageUrl=""
     body=""
+    githubLink="https://github.com/FidelDhlamini/H2O-leveler/tree/Fidel"
     />
      </div>
 
@@ -84,6 +88,7 @@ function Projects() {
     title="Taxulator App"
     imageUrl=""
     body=""
+    githubLink="https://github.com/FidelDhlamini/taxulator-app"
     />
     </div>
 
